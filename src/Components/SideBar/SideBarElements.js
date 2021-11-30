d0c4cc9cfefd60cce903bf601892efe6e3abef43
pavankumar-ts <78597728+pavankumar-ts@users.noreply.github.com>
2021-11-30 18:49:20 +0530

    slideBar

--- src/Components/SideBar/SideBarElements.js
+++ src/Components/SideBar/SideBarElements.js
@@ -40,7 +40,7 @@ export const SidebarMenu = styled.ul`
     grid-template-columns: 1fr;
     grid-template-rows: repeat(6, 80px);
     text-align:  center;
-    justify-content: center;
+    padding-left: 0px;
 
     @media screen and (max-width:480px){
     grid-template-rows: repeat(5, 60px);
